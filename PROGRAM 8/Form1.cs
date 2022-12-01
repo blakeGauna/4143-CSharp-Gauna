382251e31b6354e242ef99faf7545ed440f12145
Blake Gauna <89524476+blakeGauna@users.noreply.github.com>
2022-12-01 09:05:28 -0600

    Add files via upload

--- PROGRAM 8/Form1.cs
+++ PROGRAM 8/Form1.cs
@@ -3,54 +3,92 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
 
-namespace button
+namespace Program_8
 {
     public partial class Form1 : Form
     {
-        
+        Form2 hardwareStore = new Form2(); //Hardware Store form
+        Form3 toyStore = new Form3(); //Toy store form
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void okButton_Click(object sender, EventArgs e)
+        private void hardwareStoreToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ID = idText.Text;
-            Names = nameText.Text;
-            Price = priceText.Text;
-            Quantity = quanText.Text;
+            // Set the Parent Form of the Child window.
+            hardwareStore.MdiParent = this;
+            // Display the new form.
+            hardwareStore.Show();
         }
 
-        private void cancelButton_Click(object sender, EventArgs e)
+        private void toyStoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Set the Parent Form of the Child window.
+            toyStore.MdiParent = this;
+            // Display the new form.
+            toyStore.Show();
+        }
+
+        private void insertyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            if (hardwareStore == ActiveMdiChild) //Hardware Store form
+                hardwareStore.InsertData();
+            else if (toyStore == ActiveMdiChild) //Toy store form
+                toyStore.InsertData();
+            else
+                MessageBox.Show("Select en eligible form!");
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (hardwareStore == ActiveMdiChild) //Hardware Store form
+                hardwareStore.DeleteData();
+            else if (toyStore == ActiveMdiChild) //Toy store form
+                toyStore.DeleteData();
+            else
+                MessageBox.Show("Select en eligible form!");
+        }
+
+        private void updateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (hardwareStore == ActiveMdiChild) //Hardware Store form
+                hardwareStore.UpdateData();
+            else if (toyStore == ActiveMdiChild) //Toy store form
+                toyStore.UpdateData();
+            else
+                MessageBox.Show("Select en eligible form!");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Close();
         }
 
-        public string ID   // property
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            get { return idText.Text; }   // get method
-            set { idText.Text = value; }  // set method
+            if (hardwareStore == ActiveMdiChild) //Hardware Store form
+                hardwareStore.SaveData();
+            else if (toyStore == ActiveMdiChild) //Toy store form
+                toyStore.SaveData();
+            else
+                MessageBox.Show("Select en eligible form!");
         }
-        public string Names   // property
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            get { return nameText.Text; }   // get method
-            set { nameText.Text = value; }  // set method
-        }
-        public string Price   // property
-        {
-            get { return priceText.Text; }   // get method
-            set { priceText.Text = value; }  // set method
-        }
-        public string Quantity   // property
-        {
-            get { return quanText.Text; }   // get method
-            set { quanText.Text = value; }  // set method
+            MessageBox.Show("This program contains two store inventories that" +
+                "you have to maintain. Open a store inventory by File->Open->" +
+                " and selecting one of the two stores. Once opened, you can insert, " +
+                "delete, or update the items. Once done doing that, you have the option" +
+                " under File to save the text file. Doing do will save the text file.", "About");
         }
     }
 }
